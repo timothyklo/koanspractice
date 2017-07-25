81b311b8a161ef3ba3230e2078bedcd5a7b9e875
Timothy Lo <tim7643@gmail.com>
2017-07-25 15:29:57 -0500

    defining a triangle

--- triangle.rb
+++ triangle.rb
@@ -14,7 +14,9 @@
 #   about_triangle_project_2.rb
 #
 def triangle(a, b, c)
-  # WRITE THIS CODE
+  return :equilateral if a==b and a==c
+  return :isosceles if a==b or b==c or a==c
+  return :scalene
 end
 
 # Error class used in part 2.  No need to change this code.
